e2e3509196e024c8c24c0da88a47eaf700986df6
okym <cndd97@gmail.com>
2022-08-09 20:19:48 +0900

    dynamic import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import FooView from "../views/FooView.vue";
+import AboutView from "../views/AboutView.vue";
 
 Vue.use(VueRouter);
 
@@ -13,11 +15,16 @@ const routes: Array<RouteConfig> = [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    // component: AboutView,
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    children: [
+      {
+        path: "/about/foo",
+        name: "foo",
+        component: FooView,
+      },
+    ],
   },
 ];
 
---
+++ src/views/FooView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <button type="button" @click="changeQuery()">Change Route</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Route } from "vue-router";
+Component.registerHooks(["beforeRouteUpdate"]);
+
+@Component({})
+export default class FooView extends Vue {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  beforeRouteUpdate(to: Route, from: Route, next: any): void {
+    console.log("beforeRouteUpdate");
+    next();
+  }
+
+  changeQuery(): void {
+    this.$router.push({ query: { foo: String(Math.random()) } });
+  }
+}
+</script>
